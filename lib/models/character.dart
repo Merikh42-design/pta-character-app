8ed8de5fc81deddb480fb20635d54b1dbf897979
Merikh42-design <plur.respect@gmail.com>
2026-06-03 06:04:24 -0500

    Extend Character model to support Ancestry and Background

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,15 +1,26 @@
 class Character {
   final String? className;
+  final String? ancestry;
+  final String? background;
   final Map<String, dynamic>? startingStats;
 
-  const Character({this.className, this.startingStats});
+  const Character({
+    this.className,
+    this.ancestry,
+    this.background,
+    this.startingStats,
+  });
 
   Character copyWith({
     String? className,
+    String? ancestry,
+    String? background,
     Map<String, dynamic>? startingStats,
   }) {
     return Character(
       className: className ?? this.className,
+      ancestry: ancestry ?? this.ancestry,
+      background: background ?? this.background,
       startingStats: startingStats ?? this.startingStats,
     );
   }
