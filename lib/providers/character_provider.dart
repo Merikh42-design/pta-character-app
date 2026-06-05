161615f3ed261b633b6aa5566b77ad1879d60f39
Merikh42-design <plur.respect@gmail.com>
2026-06-04 20:11:39 -0500

    Implement ability filtering logic in CharacterNotifier when class is selected

--- lib/providers/character_provider.dart
+++ lib/providers/character_provider.dart
@@ -7,10 +7,38 @@ class CharacterNotifier extends Notifier<Character> {
   Character build() => const Character();
 
   Future<void> selectClass(String className) async {
+    // Load starting stats
     final stats = await DataService.getClassByName(className);
+
+    // Load and filter all abilities for this class
+    final freeActions = await DataService.getFreeActionsForClass(className);
+    final reactions = await DataService.getReactionsForClass(className);
+    final martialManeuvers = await DataService.getMartialManeuversForClass(className);
+    final projectileManeuvers = await DataService.getProjectileManeuversForClass(className);
+    final tacticalManeuvers = await DataService.getTacticalManeuversForClass(className);
+    final skillManeuvers = await DataService.getSkillManeuversForClass(className);
+    final attackSpells = await DataService.getAttackSpellsForClass(className);
+    final tacticalSpells = await DataService.getTacticalSpellsForClass(className);
+    final skillSpells = await DataService.getSkillSpellsForClass(className);
+    final attackChants = await DataService.getAttackChantsForClass(className);
+    final tacticalChants = await DataService.getTacticalChantsForClass(className);
+    final skillChants = await DataService.getSkillChantsForClass(className);
+
     state = state.copyWith(
       className: className,
       startingStats: stats,
+      freeActions: freeActions,
+      reactions: reactions,
+      martialManeuvers: martialManeuvers,
+      projectileManeuvers: projectileManeuvers,
+      tacticalManeuvers: tacticalManeuvers,
+      skillManeuvers: skillManeuvers,
+      attackSpells: attackSpells,
+      tacticalSpells: tacticalSpells,
+      skillSpells: skillSpells,
+      attackChants: attackChants,
+      tacticalChants: tacticalChants,
+      skillChants: skillChants,
     );
   }
 
